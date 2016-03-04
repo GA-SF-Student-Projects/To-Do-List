86d90c640c14277cb9f176cb5c12bda1eada0e82
Sam Siu <samsiu@Sams-MacBook-Pro.local>
2016-03-04 11:50:12 -0800

    Edited Layout, Fixed bug with appending to Item List

--- To-DoList/app/src/main/java/siu/example/com/to_dolist/ListItemsActivity.java
+++ To-DoList/app/src/main/java/siu/example/com/to_dolist/ListItemsActivity.java
@@ -16,6 +16,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class ListItemsActivity extends AppCompatActivity {
     public static final String TAG = "MAIN";
@@ -27,122 +29,134 @@ public class ListItemsActivity extends AppCompatActivity {
     EditText inputListItem;
     ArrayList<String> listItems;
     ArrayList<String> listSentFromMain;
-    Intent data;
-
-    Button tempButton;
-    Intent testIntent;
-    ArrayList<String> testArrayList;
+    Intent dataIntent;
+    Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
 
-        Log.d(TAG,"On Create: Loaded;");
+        Log.d(TAG, "On Create: Loaded;");
 
-        initializeActivity();
+        // Initialize Activity
+        setViews();
 
-        listItems = new ArrayList<>();
+        checkListItems();
+
+        // Handle data from MainActivity
+        dataIntent = getIntent();
+        updateListItems(dataIntent);
+
+        
         newItemAdapter = new ArrayAdapter<>(ListItemsActivity.this, android.R.layout.simple_list_item_1, listItems);
         itemsListView.setAdapter(newItemAdapter);
 
-        testIntent = getIntent();
-        if(testIntent == null){
-            Log.d("MAIN", "+++NULLL++++++"+testIntent);
-        }else{
-            listSentFromMain = testIntent.getStringArrayListExtra(MainActivity.DATA_KEY);
-            testArrayList = listSentFromMain;
-            if(listSentFromMain != null){
-                String titleFromMain = listSentFromMain.get(listSentFromMain.size()-1);
-                inputListTitle.setText(titleFromMain);
-                listSentFromMain.remove(listSentFromMain.size() - 1);
-                listItems = listSentFromMain;
-                newItemAdapter.addAll(listItems);
-                newItemAdapter.notifyDataSetChanged();
-                Log.d("MAIN", "+++++++++NOTNULL"+ listItems + "-----" + testArrayList.size());
+        // Save data to Main Activity
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveButtonClickedReturnHome();
             }
-        }
+        });
 
-        returnHomeButton();
-
-        addItemToListButton();
-
-        longItemClickStrikeThrough();
-    }
-
-    private void initializeActivity(){
-        itemsListView = (ListView)findViewById(R.id.list_item_listview);
-        addItemButton = (FloatingActionButton)findViewById(R.id.list_item_addList_button);
-        inputListTitle = (EditText)findViewById(R.id.list_item_title_edittext);
-        inputListItem = (EditText)findViewById(R.id.list_item_input_edittext);
-
-        tempButton = (Button)findViewById(R.id.list_temp_button);
-    }
-
-    private void addItemToListButton(){
+        // Add new item to list
         addItemButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String addNewItem = inputListItem.getText().toString();
-
-                if (checkListItemInput()) {
-                    Toast.makeText(ListItemsActivity.this, "Please input an item", Toast.LENGTH_SHORT).show();
-                } else {
-                    inputListItem.getText().clear();
-                    listItems.add(addNewItem);
-                    Log.d("MAIN", "----ADD ITEM TO LIST BUTTON CLICKED: " + listItems);
-                    newItemAdapter.notifyDataSetChanged();
-                }
+                addItemToListButtonClicked();
             }
         });
-    }
 
-    private void returnHomeButton() {
-        tempButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (checkListTitleInput()) {
-                    Toast.makeText(ListItemsActivity.this, "Please enter a list title", Toast.LENGTH_SHORT).show();
-                } else {
-
-                    // Append title to bottom of our list
-                    listItems.add(inputListTitle.getText().toString());
-
-                    Log.d("MAIN", "TTTTTTTTTTBUTTOM TO HOME" + listItems);
-
-                    Bundle listBundle = new Bundle();
-                    listBundle.putStringArrayList("returnList", listItems);
-
-
-                    //data = new Intent();
-                    //data.putExtra("result", inputListTitle.getText().toString());  //Send String "Surprise" with key to "result"
-                    //data.putExtra("result", listBundle);
-                    testIntent.putExtra("result", listBundle);
-                    //setResult(RESULT_OK, data);
-                    setResult(RESULT_OK, testIntent);
-                    finish();   // Go to garbage collecting and remove from memory
-                }
-            }
-        });
-    }
-
-    private void longItemClickStrikeThrough() {
+        // Strike through item in list
         itemsListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 TextView currentItem = (TextView) view;
                 strikeThroughTextView(currentItem);
-                return false;
+                return true;
             }
         });
     }
 
-    private void strikeThroughTextView(TextView currentItem){
+    private void updateListItems(Intent dataIntent){
+        if(dataIntent == null){
+            Log.d(TAG, "+++++++++getIntent() FROM MAIN IS NULL "+ dataIntent);
+        }else{
+            listSentFromMain = dataIntent.getStringArrayListExtra(MainActivity.DATA_KEY);
+            if(listSentFromMain != null){
+                String titleFromMain = listSentFromMain.get(listSentFromMain.size()-1);
+                inputListTitle.setText(titleFromMain);
+                removeTitle();
+                //listItems = new ArrayList<>(listSentFromMain.size());
+                //Collections.copy(listItems, listSentFromMain);
+                listItems = listSentFromMain;
+                Log.d(TAG, "+++++++++ListSentFromMain: NOTNULL" + listItems + "-----" + listSentFromMain.size());
+                Log.d(TAG, "+++++++++ListItems: NOTNULL" + listItems + "-----" + listItems.size());
+            }
+        }
+    }
+
+    private void checkListItems(){
+        if (listItems == null){
+            listItems = new ArrayList<>();
+        }
+    }
+
+
+
+    private void setViews(){
+        itemsListView = (ListView)findViewById(R.id.list_item_listview);
+        addItemButton = (FloatingActionButton)findViewById(R.id.list_item_addList_button);
+        inputListTitle = (EditText)findViewById(R.id.list_item_title_edittext);
+        inputListItem = (EditText)findViewById(R.id.list_item_input_edittext);
+        saveButton = (Button)findViewById(R.id.list_temp_button);
+    }
+
+
+    // Add new item to list
+    private void addItemToListButtonClicked(){
+        String addNewItem = inputListItem.getText().toString();
+
+        if (checkListItemInput()) {
+            inputListItem.setError("Pleast input an item");
+            //Toast.makeText(ListItemsActivity.this, "Please input an item", Toast.LENGTH_SHORT).show();
+        } else {
+            inputListItem.setText("");
+            listItems.add(addNewItem);
+            Log.d(TAG, "----ADD ITEM TO LIST BUTTON IS CLICKED, ITEMS IN LIST: " + listItems);
+            newItemAdapter.notifyDataSetChanged();
+        }
+    }
+
+    // Save items to Main Activity
+    private void saveButtonClickedReturnHome() {
+        if (checkListTitleInput()) {
+            //Toast.makeText(ListItemsActivity.this, "Please enter a list title", Toast.LENGTH_SHORT).show();
+            inputListTitle.setError("Please enter a list title");
+        } else {
+            // Append title to bottom of our list
+            listItems.add(inputListTitle.getText().toString());
+
+            Log.d(TAG, ">>>>>>>>>>>>BUTTON TO HOME CLICKED: SAVE ITEMS" + listItems);
+
+            Bundle listBundle = new Bundle();
+            listBundle.putStringArrayList(MainActivity.BUNDLE_KEY, listItems);
+
+            dataIntent.putExtra(MainActivity.INTENT_KEY, listBundle);
+            setResult(RESULT_OK, dataIntent);
+            finish();   // Go to garbage collecting and remove from memory
+        }
+    }
+
+    // StrikeThrough text
+    public void strikeThroughTextView(TextView currentItem){
         int paintFlags = currentItem.getPaintFlags();
-        if(paintFlags == 1281){
+        int noStrikeThroughFlag = 1281;
+        if(paintFlags == noStrikeThroughFlag){
             currentItem.setPaintFlags(paintFlags | Paint.STRIKE_THRU_TEXT_FLAG);
         } else{
-            currentItem.setPaintFlags(1281);
+            currentItem.setPaintFlags(noStrikeThroughFlag);
         }
     }
 
@@ -155,17 +169,9 @@ public class ListItemsActivity extends AppCompatActivity {
     }
 
 
-    //    private void itemClickToDescription(){
-//        itemsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                TextView currentItem = (TextView) view;
-//                currentItem.setText("Hello");
-//            }
-//        });
-//    }
-
-
+    private void removeTitle(){
+        listSentFromMain.remove(listSentFromMain.size() - 1);
+    }
 }
 
 
--- To-DoList/app/src/main/java/siu/example/com/to_dolist/MainActivity.java
+++ To-DoList/app/src/main/java/siu/example/com/to_dolist/MainActivity.java
@@ -21,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CODE = 42;
     public static final int ITEM_REQUEST_CODE = 33;
     public static final String DATA_KEY = "myDataKey";
+    public static final String INTENT_KEY = "result";
+    public static final String BUNDLE_KEY = "returnList";
+
     ListView myToDoListsView;
     FloatingActionButton addNewListButton;
     ArrayAdapter<String> myListsAdapter;
@@ -34,85 +37,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initializeActivity();
-        longItemClickStrikeThrough();
 
+        //Activity Initialization
+        setViews();
 
-        Log.d(TAG,"ON CREATE PRINT HASHMAP: " + myHashMapData);
+        // Create Intent
+        toItemActivity = new Intent(MainActivity.this, ListItemsActivity.class);
 
-        /// Setting the Adapter with new data
+        // Data: myListsData for Adapted, returnedData for HashMap
+        myListsData = new ArrayList<>();
         returnedData = new ArrayList<>();
+        myHashMapData = new HashMap<>();
+        Log.d(TAG, "ON CREATE PRINT HASHMAP: " + myHashMapData);
+
+        //Set Adapter
+        myListsAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, myListsData);
         myToDoListsView.setAdapter(myListsAdapter);
 
-        //Goes to new activity to create a new list
+        //Click FAB to create a new list
         addNewListButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Remove data from intent when clicking new list button
                 toItemActivity.removeExtra(DATA_KEY);
                 startActivityForResult(toItemActivity, REQUEST_CODE);
             }
         });
 
+        //Click item to edit list
         myToDoListsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Take clicked item as key for hashmap and return corresponding data
+                String clickedItem = ((TextView) view).getText().toString();
+                ArrayList<String> savedItemData = myHashMapData.get(clickedItem);
+                Log.d("Main", "++++++++WHEN ITEM CLICKED PRINT HASHMAP" + savedItemData);
 
-                //Get item from view, then through into hashmap, then push to an array list
-                String clickedItem = ((TextView)view).getText().toString();
-                Log.d("Main", "++++++++WHEN CLICKED PRINT HASHMAP"+myHashMapData.get(clickedItem));
-
-                toItemActivity.putStringArrayListExtra(DATA_KEY, myHashMapData.get(clickedItem));
+                toItemActivity.putStringArrayListExtra(DATA_KEY, savedItemData);
                 startActivityForResult(toItemActivity, ITEM_REQUEST_CODE);
             }
         });
-    }
 
-    private void initializeActivity(){
-        myToDoListsView = (ListView)findViewById(R.id.main_toDoListName_listView);
-        addNewListButton = (FloatingActionButton)findViewById(R.id.main_addNewList_button);
-        myListsData = new ArrayList<>();
-        myListsAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, myListsData);
-        toItemActivity = new Intent(MainActivity.this, ListItemsActivity.class);
-        myHashMapData = new HashMap<>();
-    }
-
-    // Returns data from other activity and stores to adapter
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == REQUEST_CODE){
-            if(resultCode == RESULT_OK){
-
-                Bundle returnedBundle = data.getBundleExtra("result");
-                returnedData = returnedBundle.getStringArrayList("returnList");
-                String listTitle = returnedData.get(returnedData.size()-1);
-                if(myHashMapData.containsKey(listTitle)){
-                    Log.d(TAG, "TAG ALREADY INSIDE THE HASHMAP");
-                    myHashMapData.put(listTitle, returnedData);
-                }else{
-                    myHashMapData.put(listTitle, returnedData);
-                    Log.d("Main", "===============Returned Data" + myHashMapData);
-
-                    myListsAdapter.add(listTitle);
-                    myListsAdapter.notifyDataSetChanged();
-                }
-            }
-        }
-
-        if(requestCode == ITEM_REQUEST_CODE){
-            if(resultCode == RESULT_OK){
-
-                Bundle returnedBundle = data.getBundleExtra("result");
-                returnedData = returnedBundle.getStringArrayList("returnList");
-                String listTitle = returnedData.get(returnedData.size()-1);
-                myHashMapData.put(listTitle, returnedData);
-                Log.d("Main", "===============Returned Data" + myHashMapData);
-                myListsAdapter.notifyDataSetChanged();
-            }
-        }
-    }
-
-    private void longItemClickStrikeThrough() {
+        // StrikeThrough on long item click
         myToDoListsView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
@@ -121,14 +87,59 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
     }
 
-    private void strikeThroughTextView(TextView currentItem){
+    private void setViews(){
+        myToDoListsView = (ListView)findViewById(R.id.main_toDoListName_listView);
+        addNewListButton = (FloatingActionButton)findViewById(R.id.main_addNewList_button);
+
+    }
+
+    // Returns data from other activity and stores to adapter
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Handle data when new list is clicked
+        if(requestCode == REQUEST_CODE){
+            if(resultCode == RESULT_OK){
+                Bundle returnedBundle = data.getBundleExtra(INTENT_KEY);
+                handleReturnedData(returnedBundle);
+            }
+        }
+        // Handle data returned from item click
+        if(requestCode == ITEM_REQUEST_CODE){
+            if(resultCode == RESULT_OK){
+                Bundle returnedBundle = data.getBundleExtra(INTENT_KEY);
+                handleReturnedData(returnedBundle);
+            }
+        }
+    }
+
+    private void handleReturnedData(Bundle returnedBundle){
+        returnedData = returnedBundle.getStringArrayList(BUNDLE_KEY);
+        String listTitle = returnedData.get(returnedData.size()-1);
+
+        //Check hashmap for key before displaying in main
+        if(myHashMapData.containsKey(listTitle)){
+            Log.d(TAG, "LIST TITLE ALREADY INSIDE THE HASHMAP");
+            myHashMapData.put(listTitle, returnedData);
+        }else{
+            myHashMapData.put(listTitle, returnedData);
+            Log.d(TAG, "=========LIST TITLE NOT IN HASHMAP" + myHashMapData);
+            myListsAdapter.add(listTitle);
+            myListsAdapter.notifyDataSetChanged();
+        }
+    }
+
+    public void strikeThroughTextView(TextView currentItem){
         int paintFlags = currentItem.getPaintFlags();
-        if(paintFlags == 1281){
+        int noStrikeThroughFlag = 1281;
+        if(paintFlags == noStrikeThroughFlag){
             currentItem.setPaintFlags(paintFlags | Paint.STRIKE_THRU_TEXT_FLAG);
         } else{
-            currentItem.setPaintFlags(1281);
+            currentItem.setPaintFlags(noStrikeThroughFlag);
         }
     }
 }
